b795cce89ed764bb938b67885af17f19b86d6b5b
nitish2087 <vastav2087@gmail.com>
2020-06-30 21:42:14 +0530

    union find data structure/test connectivity

---
+++ graph/union_find.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int root(vector<int>&v,int a){
+	while(v[a]!=a){
+		a=v[a];
+	}
+	return a;
+}
+
+void Union(vector<int>&v,int a,int b){
+	int root_a=root(v,a);
+	int root_b=root(v,b);
+	v[root_a]=root_b;
+}
+bool find(vector<int>&v,int a,int b){
+	if(root(v,a)==root(v,b)) return true;
+	else return false;
+}
+
+
+int main(){
+	int n,m,a,b,t1,t2;
+	cin>>n>>m;
+	vector<int>v(n);
+	for(int i=0;i<n;i++){
+		v[i]=i;
+	}
+	while(m--){
+		cin>>a>>b;
+		Union(v,a,b);
+	}
+
+	cout<<endl;
+	for(int i=0;i<n;i++){
+		cout<<v[i]<<" ";
+	}
+	cout<<endl;
+	cin>>t1>>t2;
+	cout<<find(v,t1,t2);
+	return 0;
+
+}
