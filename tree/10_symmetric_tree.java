e6dfb76a326179028f375f0d59a2e8a9c5150c65
Nitish Srivastav <nitish.srivastav@birdeye.com>
2022-06-19 13:29:05 +0530

    Symmetric tree

---
+++ tree/10_symmetric_tree.java
@@ -0,0 +1,47 @@
+// Symmetric tree
+
+/*
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+Constraints:
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean symCheck(TreeNode right, TreeNode left) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;   
+        return symCheck(right.left, left.right) && symCheck(right.right, left.left);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        if (root.left == null && root.right == null) return true;
+        if (root.left == null || root.right == null) return false;
+        return symCheck(root.right, root.left);
+        
+    }
+}
