6a2873e136fe7faaa9f408de723ec271cf9deb7e
Nitish Srivastav <nitish.srivastav@birdeye.com>
2022-06-19 12:17:21 +0530

    Renaming files and adding Binary tree postorder traversal

---
+++ tree/6_binary_tree_inorder_traversal.java
@@ -0,0 +1,56 @@
+// Binary tree inorder traversal
+
+/*
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void inOrder(TreeNode root, List<Integer> result) {
+        if (root != null) {
+            inOrder(root.left, result);
+            result.add(root.val);
+            inOrder(root.right, result);
+        }
+    }
+    
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        inOrder(root, result);
+        return result;
+    }
+}
