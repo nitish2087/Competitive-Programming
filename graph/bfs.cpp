f6b8e3efdc5600b5f13046603e3dcf7861d36aab
Nitish Srivastav <vastav2087@gmail.com>
2022-09-11 19:23:21 +0530

    updating solution

--- graph/bfs.cpp
+++ graph/bfs.cpp
@@ -1,32 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void bfs(vector<int>v[],int s){
-	vector<bool>visited={false};
- 	queue<int>q;
- 	q.push(s);
- 	visited[s]=true;
- 	while(!q.empty()){
- 		int t=q.front();
- 		cout<<t<<" ";
+void bfs(vector<int>v[], int root){
+	vector<bool> visited = {false}; // visited array to avoid cycle cases, not required in tree
+ 	queue<int> q; 
+ 	q.push(root);
+ 	visited[root] = true;
+ 	while (!q.empty()) {
+ 		int t = q.front();
+ 		cout << t << " ";
  		q.pop();
- 		for(auto i=v[t].begin();i<v[t].end();i++){
- 			if(!visited[*i]){
+ 		for (auto i = v[t].begin(); i < v[t].end(); i++){
+ 			if (!visited[*i]) {
  				q.push(*i);
- 				visited[*i]=true;
+ 				visited[*i] = true;
  			}
  		}
  	}
 }
  int main(){
- 	int n,m,a,b;
- 	cin>>n>>m;
- 	vector<int>v[n];
- 	while(m--){
- 		cin>>a>>b;
+ 	int n, m, a, b;
+ 	cin >> n >> m;
+ 	vector<int> v[n];
+ 	while(m--) {
+ 		cin >> a >> b;
  		v[a].push_back(b);
  		v[b].push_back(a);
  	}
- 	bfs(v,0);
+ 	bfs(v, 0); // 0 -> root node
  	return 0;
  }
